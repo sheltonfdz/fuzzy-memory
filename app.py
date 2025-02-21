d77efbf860571859c5637ed8c10312b3364b097e
sheltonfdz <108753927+sheltonfdz@users.noreply.github.com>
2025-02-21 11:05:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,4 +32,6 @@ def predict():
         return jsonify({"error": str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5000)
+
